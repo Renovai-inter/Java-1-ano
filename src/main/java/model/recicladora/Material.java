bd6a65d57bf321eb1b84798473d7af5543b7139a
Luiza Caravaggio <luiza.20260177@institutojef.ogr.br>
2026-08-24 10:49:50 -0300

    fazendo os getters e setters recicladora

--- src/main/java/model/recicladora/Material.java
+++ src/main/java/model/recicladora/Material.java
@@ -15,4 +15,44 @@ public class Material {
         this.quantidadeSolicitada = quantidadeSolicitada;
         this.valorPorKg = valorPorKg;
     }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getQuantidadeSolicitada() {
+        return quantidadeSolicitada;
+    }
+
+    public void setQuantidadeSolicitada(double quantidadeSolicitada) {
+        this.quantidadeSolicitada = quantidadeSolicitada;
+    }
+
+    public double getValorPorKg() {
+        return valorPorKg;
+    }
+
+    public void setValorPorKg(double valorPorKg) {
+        this.valorPorKg = valorPorKg;
+    }
 }
